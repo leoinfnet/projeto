ecb58ca4a9a774c8aea8abee2aa22c6a4ac057fe
Leo Gloria <contato@leonardogloria.com.br>
2023-10-26 07:27:30 -0300

    Implementando calculo de media

--- Teste2.java
+++ Teste2.java
@@ -3,4 +3,8 @@ public class Teste2 {
         System.out.println("Teste2 Corrigido");
         System.out.println("Teste22 Corrigido");
     }
+    public int calcularMedia(){
+        System.out.println("Calculando a media");
+        return 0;
+    }
 }
