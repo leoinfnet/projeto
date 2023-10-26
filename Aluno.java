75d5d6ec99d3be95d39f7daa8a52f5f7bc0df3db
Leo Gloria <contato@leonardogloria.com.br>
2023-10-26 08:36:47 -0300

    Excluindo arquivos

--- Aluno.java
+++
@@ -1,8 +0,0 @@
-public class Aluno {
-    private String nome;
-    private String cpf;
-
-    public void cadastrar(){
-        System.out.println("Cadastrar Aluno");
-    }
-}
--- Teste.java
+++
@@ -1,6 +0,0 @@
-public class Teste {
-    public static void main(String [] args){
-        String nome = "Teste2";
-        String nome2=  "QQ coisa";
-    }
-}
