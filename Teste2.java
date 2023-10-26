be3987745dd80ef34f904fd2c9450c7a24eaf6b2
Leo Gloria <contato@leonardogloria.com.br>
2023-10-26 09:13:43 -0300

    Bug nao tao critico

--- Teste2.java
+++ Teste2.java
@@ -2,6 +2,7 @@ public class Teste2 {
     public static void main(String [] args){
         System.out.println("Teste2 Corrigido");
         System.out.println("Teste Corrigido12");
+        System.out.println("Teste Corrigido1232131");
     }
     public int calcularMedia(){
         System.out.println("Calculando a media");
