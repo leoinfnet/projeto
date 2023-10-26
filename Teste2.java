d550c4f80151901503920ca8ecad9da16b4acfa4
Leo Gloria <contato@leonardogloria.com.br>
2023-10-26 08:57:30 -0300

    Revert "Bug 879"
    This reverts commit 0bf2eea321db9397a0461704e07167e88164fbae.

--- Teste2.java
+++ Teste2.java
@@ -1,7 +1,7 @@
 public class Teste2 {
     public static void main(String [] args){
         System.out.println("Teste2 Corrigido");
-        System.out.println("Teste Corrigido1123212");
+        System.out.println("Teste Corrigido12");
     }
     public int calcularMedia(){
         System.out.println("Calculando a media");
