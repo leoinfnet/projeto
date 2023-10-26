1f81866221fb860006a73485cff930185e337d90
Leonardo Silva da Gloria <leonardo.gloria@prof.infnet.edu.br>
2023-10-26 08:00:45 -0300

    Adicionando titulo do metodo e corrigindo bug

--- Teste2.java
+++ Teste2.java
@@ -1,13 +1,13 @@
 public class Teste2 {
     public static void main(String [] args){
         System.out.println("Teste2 Corrigido");
-        System.out.println("Teste45 Corrigido");
+        System.out.println("Teste454545 Corrigido");
     }
     public int calcularMedia(){
         System.out.println("Calculando a media");
         return 0;
     }
-    public int calcularMedia(){
+    public int calcularMediaMediana(){
         System.out.println("Calculando a mediana");
         return 0;
     }
