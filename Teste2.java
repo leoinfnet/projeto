6628de0c3d5f5bc9dc8479495f3f7ccefb4e0abd
Leo Gloria <contato@leonardogloria.com.br>
2023-10-26 08:00:15 -0300

    Corrigindo bug

--- Teste2.java
+++ Teste2.java
@@ -1,7 +1,7 @@
 public class Teste2 {
     public static void main(String [] args){
         System.out.println("Teste2 Corrigido");
-        System.out.println("Teste45 Corrigido");
+        System.out.println("Teste Corrigido");
     }
     public int calcularMedia(){
         System.out.println("Calculando a media");
